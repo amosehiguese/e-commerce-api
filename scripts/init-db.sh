08ad08f5b3ce2c4d7a02b8239a2d8ba31ece31d1
amosehiguese <amosehiguese@gmail.com>
2024-12-25 09:36:28 +0100

    set up init-db script to spin up a postgres container

---
+++ scripts/init-db.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -x
+set -eo pipefail
+
+SKIP_DOCKER=$1
+
+DATABASE_USER="${DB_USER}"
+DATABASE_PASSWORD="${DB_PASSWORD}"
+DATABASE_NAME="${DB_NAME}"
+DATABASE_PORT="${DB_PORT}"
+
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run --rm -d --name postgresql \
+    -p ${DATABASE_PORT}:5432 \
+    -e POSTGRES_PASSWORD=${DATABASE_PASSWORD} \
+    -e POSTGRES_DB=${DATABASE_NAME} \
+    -e POSTGRES_USER=${DATABASE_USER} \
+    postgres:15.2
+fi
+
+export PGPASSWORD="${DATABASE_PASSWORD}"
+until docker exec -it postgresql psql -U "${DATABASE_USER}" "${DATABASE_NAME}" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 3
+done
+
+>&2 echo "Postgres is up and running on port ${DATABASE_PORT}"
+
